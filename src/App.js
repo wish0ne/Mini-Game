e1b7e508dc863c0add191d94acd075f073e3d423
wish1030 <wish@khu.ac.kr>
2021-05-24 22:39:50 +0900

    Fix : github page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./home/home";
 import Card from "./card/card.js";
 import Snake from "./Snake/Snake.js";
+import { introPath } from "./constant";
 
 function App() {
   return (
     <>
       <Router>
-        <Route path="/" exact component={Home} />
-        <Route path="/card" component={Card} />
-        <Route path="/snake" component={Snake} />
+        <Route path={introPath + "/"} exact component={Home} />
+        <Route path={introPath + "/card"} component={Card} />
+        <Route path={introPath + "/snake"} component={Snake} />
       </Router>
     </>
   );
