352f16a8b15e1c5edb17a8d8d565511750839a73
wish1030 <wish@khu.ac.kr>
2021-05-24 22:14:30 +0900

    Fix : github page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./home/home";
-import Card from "./card/Card.js";
+import Card from "./card/card.js";
 import Snake from "./Snake/Snake.js";
 
 function App() {
