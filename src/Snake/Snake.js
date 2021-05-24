ff74789350cfecd4ff457ea930868dce2ab7bb8f
wish1030 <wish@khu.ac.kr>
2021-05-24 16:24:00 +0900

    Snake game 구현

--- src/Snake/Snake.js
+++ src/Snake/Snake.js
@@ -1,41 +1,161 @@
 import "./Snake.css";
 import { Link } from "react-router-dom";
-import { useState } from "react";
-
-let board = [];
-for (let i = 0; i < 20; i++) {
-  board.push([]);
-  for (let j = 0; j < 20; j++) {
-    board[i].push(0);
-  }
-}
+import { useState, useRef, useEffect } from "react";
 
 function Snake() {
+  let board = [];
+  for (let i = 0; i < 30; i++) {
+    board.push([]);
+    for (let j = 0; j < 30; j++) {
+      board[i].push("none");
+    }
+  }
+
+  const randomPosition = () => {
+    const rPosition = {
+      x: Math.floor(Math.random() * 30),
+      y: Math.floor(Math.random() * 30),
+    };
+    return rPosition;
+  };
+
   const [boards, setBoards] = useState(board);
-  const [position, setPosition] = useState([{ x: 0, y: 0 }]);
+  const [position, setPosition] = useState([{ x: 10, y: 10 }]);
+  const [direction, setDirection] = useState("right");
+  const [food, setFood] = useState(randomPosition);
+  const [score, setScore] = useState(0);
+  const [end, setEnd] = useState(false);
+
+  const popup = useRef();
 
   const makeBoard = boards.map((row, index) => {
     return (
       <tr key={index} className="Snake-Row">
         {row.map((cell, index) => {
-          return (
-            <td key={index} className="Snake-Cell">
-              0
-            </td>
-          );
+          switch (cell) {
+            case "none":
+              return <td key={index} className="Snake-Cell"></td>;
+            case "snake":
+              return <td key={index} className="Snake-Cell snake"></td>;
+            case "food":
+              return <td key={index} className="Snake-Cell food"></td>;
+          }
         })}
       </tr>
     );
   });
+
+  const displaySnake = () => {
+    const newBoards = board;
+    position.forEach((cell) => {
+      newBoards[cell.x][cell.y] = "snake";
+    });
+    newBoards[food.x][food.y] = "food";
+
+    setBoards(newBoards);
+  };
+
+  const changeDirection = (e) => {
+    if (e.key === "ArrowRight") {
+      setDirection("right");
+    } else if (e.key === "ArrowLeft") {
+      setDirection("left");
+    } else if (e.key === "ArrowUp") {
+      setDirection("up");
+    } else if (e.key === "ArrowDown") {
+      setDirection("down");
+    }
+  };
+
+  window.addEventListener("keydown", changeDirection);
+
+  const moveSnake = () => {
+    const newSnake = [];
+    if (direction === "right") {
+      newSnake.push({ x: position[0].x, y: (position[0].y + 1) % 30 });
+    } else if (direction === "left") {
+      newSnake.push({ x: position[0].x, y: (position[0].y - 1 + 30) % 30 });
+    } else if (direction === "up") {
+      newSnake.push({ x: (position[0].x - 1 + 30) % 30, y: position[0].y });
+    } else if (direction === "down") {
+      newSnake.push({ x: (position[0].x + 1) % 30, y: position[0].y });
+    }
+
+    position.forEach((cell) => {
+      newSnake.push(cell);
+    });
+
+    for (let i = 1; i < newSnake.length; i++) {
+      if (newSnake[0].x === newSnake[i].x && newSnake[0].y === newSnake[i].y) {
+        setEnd(true);
+        setTimeout(gameOver, 500);
+      }
+    }
+
+    if (newSnake[0].x === food.x && newSnake[0].y === food.y) {
+      setFood(randomPosition);
+      setScore(score + 10);
+    } else {
+      newSnake.pop();
+    }
+    setPosition(newSnake);
+    displaySnake();
+  };
+
+  useInterval(moveSnake, 100);
+
+  function useInterval(callback, delay) {
+    const savedCallback = useRef();
+
+    // // Remember the latest callback.
+    useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+
+    // Set up the interval.
+    useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+      if (!end) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [end]);
+  }
+
+  const gameOver = () => {
+    popup.current.classList.remove("hidden");
+  };
 
   return (
     <div className="Snake">
       <table className="Snake-Board">
         <tbody>{makeBoard}</tbody>
       </table>
+      <div className="Snake-Score">
+        <div>Score</div>
+        {score}
+      </div>
       <Link to="/">
         <button className="Snake-button">🔙Back</button>
       </Link>
+      <div className="Snake-popup hidden" ref={popup}>
+        <div className="Snake-popup_layer">
+          <div className="text_area">
+            Score
+            <div>{score}</div>
+          </div>
+          <div className="btn_area">
+            <Link to="/">
+              <button type="button" name="button" className="Snake-exit">
+                exit
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="popup_dimmed"></div>
+      </div>
     </div>
   );
 }
